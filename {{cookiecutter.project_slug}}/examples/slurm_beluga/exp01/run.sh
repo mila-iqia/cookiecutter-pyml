c1682036c3e7e7ffec4f3d1f7b538b48b0e780b5
Mirko Bronzi <m.bronzi@gmail.com>
2019-11-07 13:30:14 -0500

    created separated exp. for orion/non-orion

--- {{cookiecutter.project_slug}}/examples/slurm_beluga/exp01/run.sh
+++ {{cookiecutter.project_slug}}/examples/slurm_beluga/exp01/run.sh
@@ -13,9 +13,6 @@
 ##SBATCH --mail-user={{ cookiecutter.email }}
 
 export MLFLOW_TRACKING_URI='../mlruns'
-# the following is used only with orion
-export ORION_DB_ADDRESS='../orion_db.pkl'
-export ORION_DB_TYPE='pickleddb'
 
 # 2. Copy your dataset on the compute node
 export DATADIR=$SLURM_TMPDIR/dataset
@@ -25,9 +22,7 @@ time tar xf $SLURM_TMPDIR/dataset.tar -C $SLURM_TMPDIR/ --strip=4
 # 3. Launch your job, tell it to save the model in $SLURM_TMPDIR/output
 #    and look for the dataset into $SLURM_TMPDIR/output
 # use orion..
-orion -v hunt --config ../orion_config.yaml ../../../{{cookiecutter.project_slug}}/main.py --data no_data --output $SLURM_TMPDIR/no_output --config ../config.yaml
-# or not
-# main --data no_data --output $SLURM_TMPDIR/no_output --config ../config.yaml
+main --data no_data --output $SLURM_TMPDIR/no_output --config ../config.yaml
 
 # 4. Copy whatever you want to save on $SCRATCH
 # rsync -avz $SLURM_TMPDIR/output /network/tmp1/${USER}/
--- {{cookiecutter.project_slug}}/examples/slurm_beluga_orion/exp01/run.sh
+++ {{cookiecutter.project_slug}}/examples/slurm_beluga_orion/exp01/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # __TODO__ fix options if needed
-#SBATCH --partition=long
+#SBATCH --account=rpp-bengioy
 #SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
 #SBATCH --mem=5G
 #SBATCH --time=0:05:00
 #SBATCH --job-name={{ cookiecutter.project_slug }}
@@ -12,16 +13,18 @@
 ##SBATCH --mail-user={{ cookiecutter.email }}
 
 export MLFLOW_TRACKING_URI='../mlruns'
+export ORION_DB_ADDRESS='../orion_db.pkl'
+export ORION_DB_TYPE='pickleddb'
 
 # 2. Copy your dataset on the compute node
-#export DATADIR=$SLURM_TMPDIR/dataset
-#time rsync -a --info=progress2 /__TODO__/path/to/dataset.tar $SLURM_TMPDIR/
-#time tar xf $SLURM_TMPDIR/dataset.tar -C $SLURM_TMPDIR/ --strip=4
+export DATADIR=$SLURM_TMPDIR/dataset
+time rsync -a --info=progress2 /__TODO__/path/to/dataset.tar $SLURM_TMPDIR/
+time tar xf $SLURM_TMPDIR/dataset.tar -C $SLURM_TMPDIR/ --strip=4
 
 # 3. Launch your job, tell it to save the model in $SLURM_TMPDIR/output
 #    and look for the dataset into $SLURM_TMPDIR/output
 # use orion..
-main --data no_data --output $SLURM_TMPDIR/no_output --config ../config.yaml
+orion -v hunt --config ../orion_config.yaml ../../../{{cookiecutter.project_slug}}/main.py --data no_data --output $SLURM_TMPDIR/no_output --config ../config.yaml
 
 # 4. Copy whatever you want to save on $SCRATCH
 # rsync -avz $SLURM_TMPDIR/output /network/tmp1/${USER}/
