5e16500e5e724720e01664d5ab4d19eda3e166dd
Mirko Bronzi <m.bronzi@gmail.com>
2020-02-05 15:23:08 -0500

    added flake8 check (#11)
    (plus travis config file refactoring)

--- tests/end2end_pytorch/run.sh
+++ tests/end2end_pytorch/run.sh
@@ -1,6 +1,18 @@
+# exit at the first error
+set -e
+# go to the test folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}
+
 cookiecutter ../.. --no-input --output-dir=./
 cd wonderful_project
 pip install -e . --quiet
+
+#run flake8 test first
+pip install flake8 --quiet
+sh config/hooks/pre-commit
+
+# run the example
 cd examples/local
 pwd
 main --data data --output output --config config.yaml --disable_progressbar
--- tests/end2end_tensorflow/run.sh
+++ tests/end2end_tensorflow/run.sh
@@ -1,7 +1,20 @@
+# exit at the first error
+set -e
+# go to the test folder
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${DIR}
+
 python -c "from cookiecutter.main import cookiecutter; cookiecutter('../..', no_input=True, extra_context={'dl_framework': 'tensorflow_cpu'}, output_dir='./')"
 cd wonderful_project
 pip install -e . --quiet
+# necessary cause tf dependencies are sometimes not updated
 pip install -U setuptools numpy six --quiet
+
+#run flake8 test first
+pip install flake8 --quiet
+sh config/hooks/pre-commit
+
+# run the example
 cd examples/local
 pwd
 main --data data --output output --config config.yaml --disable_progressbar
