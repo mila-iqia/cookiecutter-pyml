b00714805ffb7d2bb23f68f7fb4f70f7b86a4061
mirkobronzi <m.bronzi@gmail.com>
2020-07-02 15:55:57 -0400

    changed --disable_progressbar => --disable-progressbar

--- {{cookiecutter.project_slug}}/examples/local_orion/run.sh
+++ {{cookiecutter.project_slug}}/examples/local_orion/run.sh
@@ -4,4 +4,5 @@ export ORION_DB_TYPE='pickleddb'
 orion -v hunt --config orion_config.yaml ../../{{cookiecutter.project_slug}}/main.py --data data \
     --config config.yaml --disable_progressbar \
     --output '{exp.working_dir}/{exp.name}_{trial.id}/' \
-    --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log'
+    --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log' \
+     --disable-progressbar
