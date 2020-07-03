8faeb6cca01279e4bcb3682a28db30ae5afc70bc
mirkobronzi <m.bronzi@gmail.com>
2020-07-03 09:42:22 -0400

    restored placeholder

--- {{cookiecutter.project_slug}}/examples/slurm_mila/to_submit.sh
+++ {{cookiecutter.project_slug}}/examples/slurm_mila/to_submit.sh
@@ -4,12 +4,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=5G
 #SBATCH --time=0:05:00
-#SBATCH --job-name=my_proj
+#SBATCH --job-name={{ cookiecutter.project_slug }}
 #SBATCH --output=logs/out_%a.log
 #SBATCH --error=logs/err_%a.log
 # remove one # if you prefer receiving emails
 ##SBATCH --mail-type=all
-##SBATCH --mail-user=john.doe@example.com
+##SBATCH --mail-user={{ cookiecutter.email }}
 
 export MLFLOW_TRACKING_URI='mlruns'
 
