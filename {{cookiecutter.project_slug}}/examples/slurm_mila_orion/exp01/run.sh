3f41fa197726fca26fd7eed8c175c6246467fb20
mirkobronzi <m.bronzi@gmail.com>
2020-07-03 10:46:25 -0400

    fix to orion exp on slurm

--- {{cookiecutter.project_slug}}/examples/slurm_mila_orion/exp01/run.sh
+++ {{cookiecutter.project_slug}}/examples/slurm_mila_orion/exp01/run.sh
@@ -1,25 +1,5 @@
-#!/bin/bash
-# __TODO__ fix options if needed
-#SBATCH --partition=long
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=5G
-#SBATCH --time=0:05:00
-#SBATCH --job-name={{ cookiecutter.project_slug }}
-#SBATCH --output=logs/out_%a.log
-#SBATCH --error=logs/err_%a.log
-# remove one # if you prefer receiving emails
-##SBATCH --mail-type=all
-##SBATCH --mail-user={{ cookiecutter.email }}
-
-export MLFLOW_TRACKING_URI='../mlruns'
-export ORION_DB_ADDRESS='../orion_db.pkl'
-export ORION_DB_TYPE='pickleddb'
-
-orion -v hunt --config ../orion_config.yaml ../../../{{cookiecutter.project_slug}}/main.py \
---data no_data --output $SLURM_TMPDIR/no_output --config ../config.yaml \
---log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log'
-
-orion -v hunt --config ../orion_config.yaml ../../../{{cookiecutter.project_slug}}/main.py \
-    --data data --config ../config.yaml --disable_progressbar \
-    --output '{exp.working_dir}/{exp.name}_{trial.id}/' \
-    --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log'
+rm -fr logs
+mkdir logs
+folder=`pwd`
+name=`basename ${folder}`
+sbatch --job-name ${name} to_submit.sh
