2af6754c0bf423aa600f5c3ecbec277c51cca986
mirkobronzi <m.bronzi@gmail.com>
2020-07-27 16:40:29 -0400

    added docstring

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/file_utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/file_utils.py
@@ -5,7 +5,12 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 
-def rsync_folder(source, target):
+def rsync_folder(source, target):  # pragma: no cover
+    """Uses rsync to copy the content of source into target.
+
+    :param source: (str) path to the source folder.
+    :param target: (str) path to the target folder.
+    """
 
     if not os.path.exists(target):
         os.makedirs(target)
