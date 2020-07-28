ea963be172b98a1f00cfd01fec0d4215dd57f992
mirkobronzi <m.bronzi@gmail.com>
2020-07-28 15:02:39 -0400

    added command to set the seed in tensorflow

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/reproducibility_utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/reproducibility_utils.py
@@ -4,7 +4,9 @@ import numpy as np
 {%- if cookiecutter.dl_framework == 'pytorch' %}
 import torch
 {%- endif %}
-
+{%- if cookiecutter.dl_framework in ['tensorflow_cpu', 'tensorflow_gpu'] %}
+import tensorflow as tf
+{%- endif %}
 
 def set_seed(seed):  # pragma: no cover
     """Seed the provided seed in python/numpy/DL framework.
@@ -18,4 +20,7 @@ def set_seed(seed):  # pragma: no cover
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+{%- endif %}
+{%- if cookiecutter.dl_framework in ['tensorflow_cpu', 'tensorflow_gpu'] %}
+    tf.random.set_seed(seed)
 {%- endif %}
