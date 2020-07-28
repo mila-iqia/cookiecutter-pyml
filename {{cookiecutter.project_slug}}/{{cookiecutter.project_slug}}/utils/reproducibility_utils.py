3f2ab5bb673b30633c8a8cea21dd534ddc0cccd1
mirkobronzi <m.bronzi@gmail.com>
2020-07-28 13:16:35 -0400

    removed pytorch part from tensorflow

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/reproducibility_utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/reproducibility_utils.py
@@ -13,7 +13,9 @@ def set_seed(seed):  # pragma: no cover
     """
     random.seed(seed)
     np.random.seed(seed)
+{ % - if cookiecutter.dl_framework == 'pytorch' %}
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+{%- endif %}
