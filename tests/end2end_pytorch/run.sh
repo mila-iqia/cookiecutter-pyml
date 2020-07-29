c8e754fd10d43d2e7305a4b13db01528196026b5
mirkobronzi <m.bronzi@gmail.com>
2020-07-29 13:44:11 -0400

    added test to check repro using Orion

--- tests/end2end_pytorch/run.sh
+++ tests/end2end_pytorch/run.sh
@@ -38,3 +38,12 @@ fi
 cd ../..
 cd examples/local_orion
 sh run.sh
+mv orion_working_dir orion_working_dir_OLD
+# re-run the example to check reproducibility
+rm -fr orion_db*
+sh run.sh
+DIFF_LINES=`grep "best_dev_metric" orion_working_dir*/*/stats.yaml | sed 's@^.*best_dev_metric: @@g' | sort | uniq | wc -l`
+if [ ${DIFF_LINES} -gt 2 ]; then
+    echo "ERROR: two identical Orion runs produced different output results - review seed implementation"
+    exit 1
+fi
