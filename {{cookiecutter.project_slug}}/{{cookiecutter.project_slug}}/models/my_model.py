8ec0341a0f776f02d296cfb6fbefed3beea3ad4e
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2021-03-30 11:49:54 -0400

    Update torch model to pl-lightning model

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/models/my_model.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/models/my_model.py
@@ -4,7 +4,10 @@ import logging
 import tensorflow as tf
 {%- endif %}
 {%- if cookiecutter.dl_framework == 'pytorch' %}
-import torch.nn as nn
+import torch
+import pytorch_lightning as pl
+
+from {{cookiecutter.project_slug}}.models.model_loader import load_loss, load_optimizer
 {%- endif %}
 
 from {{cookiecutter.project_slug}}.utils.hp_utils import check_and_log_hp
@@ -12,7 +15,7 @@ from {{cookiecutter.project_slug}}.utils.hp_utils import check_and_log_hp
 logger = logging.getLogger(__name__)
 
 
-class MyModel({%- if cookiecutter.dl_framework == 'pytorch' %}nn.Module{%- else %}tf.keras.Model{%- endif %}):  # pragma: no cover
+class MyModel({%- if cookiecutter.dl_framework == 'pytorch' %}pl.LightningModule{%- else %}tf.keras.Model{%- endif %}):  # pragma: no cover
     """Simple Model Class.
 
     Inherits from the given framework's model class. This is a simple MLP model.
@@ -27,8 +30,6 @@ class MyModel({%- if cookiecutter.dl_framework == 'pytorch' %}nn.Module{%- else 
         super(MyModel, self).__init__()
 
         check_and_log_hp(['size'], hyper_params)
-        self.hyper_params = hyper_params
-
         {%- if cookiecutter.dl_framework in ['tensorflow_cpu', 'tensorflow_gpu'] %}
         self.hyper_params = hyper_params
         self.dense1 = tf.keras.layers.Dense(hyper_params['size'])
@@ -45,8 +46,24 @@ class MyModel({%- if cookiecutter.dl_framework == 'pytorch' %}nn.Module{%- else 
         return hidden2
         {%- endif %}
         {%- if cookiecutter.dl_framework == 'pytorch' %}
-        self.linear1 = nn.Linear(5, hyper_params['size'])
-        self.linear2 = nn.Linear(hyper_params['size'], 1)
+        self.save_hyperparameters(hyper_params)  # they will become available via model.hparams
+        self.linear1 = torch.nn.Linear(5, hyper_params['size'])
+        self.linear2 = torch.nn.Linear(hyper_params['size'], 1)
+        self.loss_fn = load_loss(hyper_params)  # 'load_loss' could be part of the model itself...
+
+    def configure_optimizers(self):
+        """Returns the combination of optimizer(s) and learning rate scheduler(s) to train with.
+
+        Here, we read all the optimization-related hyperparameters from the config dictionary and
+        create the required optimizer/scheduler combo.
+
+        This function will be called automatically by the pytorch lightning trainer implementation.
+        See https://pytorch-lightning.readthedocs.io/en/latest/common/optimizers.html for more info
+        on the expected returned elements.
+        """
+        # we use the generic loading function from the `model_loader` module, but it could be made
+        # a direct part of the model (useful if we want layer-dynamic optimization)
+        return load_optimizer(hyper_params, self)
 
     def forward(self, data):
         """Forward method of the model.
@@ -58,7 +75,36 @@ class MyModel({%- if cookiecutter.dl_framework == 'pytorch' %}nn.Module{%- else 
             tensor: the output of the model computation.
 
         """
-        hidden = nn.functional.relu(self.linear1(data))
+        hidden = torch.nn.functional.relu(self.linear1(data))
         result = self.linear2(hidden)
         return result.squeeze()
+
+    def _generic_step(
+            self,
+            batch: typing.Any,
+            batch_idx: int,
+    ) -> typing.Any:
+        """Runs the prediction + evaluation step for training/validation/testing."""
+        input_data, targets = batch
+        preds = self(input_data)  # calls the forward pass of the model
+        loss = self.loss_fn(preds, targets)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        """Runs a prediction step for training, returning the loss."""
+        loss = self._generic_step(batch, batch_idx)
+        self.log("train_loss", loss)
+        self.log("epoch", self.current_epoch)
+        self.log("step", self.global_step)
+        return loss  # this function is required, as the loss returned here is used for backprop
+
+    def validation_step(self, batch, batch_idx):
+        """Runs a prediction step for validation, logging the loss."""
+        loss = self._generic_step(batch, batch_idx)
+        self.log("val_loss", loss)
+
+    def test_step(self, batch, batch_idx):
+        """Runs a prediction step for testing, logging the loss."""
+        loss = self._generic_step(batch, batch_idx)
+        self.log("test_loss", loss)
         {%- endif %}
