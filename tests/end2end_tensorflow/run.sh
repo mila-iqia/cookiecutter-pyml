0a0135b9fa79f3b99f633579a25e7827ba5ce55d
mirkobronzi <m.bronzi@gmail.com>
2021-04-26 09:59:30 -0400

    setting fake name/email for git.

--- tests/end2end_tensorflow/run.sh
+++ tests/end2end_tensorflow/run.sh
@@ -7,6 +7,9 @@ cd ${DIR}
 python -c "from cookiecutter.main import cookiecutter; cookiecutter('../..', no_input=True, extra_context={'dl_framework': 'tensorflow_cpu'}, output_dir='./')"
 cd wonderful_project
 git init
+# setting some fake name/email for git:
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 git add -A
 git commit -m "initial commit"
 pip install -e . --quiet
