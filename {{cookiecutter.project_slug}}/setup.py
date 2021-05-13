14c6b36c576a8ac60814ac58bcd44f266c31595d
mirkobronzi <m.bronzi@gmail.com>
2021-05-13 10:09:29 -0400

    removing more tf dependencies.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -31,11 +31,6 @@ setup(
         {%- if cookiecutter.dl_framework == 'tensorflow_gpu' %}
         'tensorflow-gpu==2.4.0'
         {%- endif %}
-        {%- if cookiecutter.dl_framework in ['tensorflow_cpu', 'tensorflow_gpu'] %}
-        'scipy==1.4.1',
-        'setuptools>=41.0.0',
-        'six>=1.12.0'],
-        {%- endif %}
     entry_points={
         'console_scripts': [
             'main={{ cookiecutter.project_slug }}.main:main'
