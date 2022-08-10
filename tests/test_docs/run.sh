d39bc30bef978f21f7fca5c14182dabb291e0d72
Jeremy Pinto <jerpint@gmail.com>
2022-08-10 18:32:51 -0400

    Update dependency install in documentation test

--- tests/test_docs/run.sh
+++ tests/test_docs/run.sh
@@ -6,12 +6,11 @@ cd ${DIR}
 
 cookiecutter ../.. --no-input --output-dir=./
 cd wonderful_project
-pip install -e . --quiet
+pip install -e .'[dev]' --quiet
 
 # necessary cause tf dependencies are sometimes not updated
 pip install -U setuptools numpy six --quiet
 
 # Build the docs
 cd docs
-pip install -r requirements.txt
 sphinx-build -b html -d _build/doctrees . _build/html
