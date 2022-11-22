8abfdaadd59c2c7726eac8881bfa165c08b40d74
Your Name <you@example.com>
2022-11-22 15:30:00 -0500

    update torchvision version

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -26,7 +26,7 @@ setup(
         'tensorboard==2.9.1',
         'tqdm==4.64.0',
         'torch==1.12.0',
-        'torchvision==0.14.0'
+        'torchvision==0.13.0'
     ],
     entry_points={
         'console_scripts': [
