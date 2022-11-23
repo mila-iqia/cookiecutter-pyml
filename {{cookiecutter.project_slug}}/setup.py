5ed8f79811ffe00272c48ecb52be981989a7f64a
Jeremy Pinto <jerpint@gmail.com>
2022-11-23 09:25:07 -0500

    Add missing comma

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -26,8 +26,8 @@ setup(
         'tensorboard==2.9.1',
         'tqdm==4.64.0',
         'torch==1.12.0',
-        'torchmetrics==0.10.3'
-        'torchvision==0.13.0'
+        'torchmetrics==0.10.3',
+        'torchvision==0.13.0',
     ],
     entry_points={
         'console_scripts': [
