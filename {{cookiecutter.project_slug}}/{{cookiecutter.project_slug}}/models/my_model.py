ae5852c972750612209f3180ae53a9e0ce6fbe2c
Jeremy Pinto <jerpint@gmail.com>
2022-11-28 11:14:37 -0500

    linting

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/models/my_model.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/models/my_model.py
@@ -111,7 +111,7 @@ class SimpleMLP(BaseModel):  # pragma: no cover
             hparams
         )  # 'load_loss' could be part of the model itself...
 
-        self.example_input_array = torch.rand((1, 1, 28, 28)) # will allow logging graph
+        self.example_input_array = torch.rand((1, 1, 28, 28))  # will allow logging graph
         self.flatten = nn.Flatten()
         self.mlp_layers = nn.Sequential(
             nn.Linear(
@@ -148,7 +148,7 @@ class SimpleCNN(BaseModel):  # pragma: no cover
         check_and_log_hp(["hidden_dim", "num_classes"], hparams)
         num_classes = hparams["num_classes"]
         hidden_dim = hparams["hidden_dim"]
-        self.example_input_array = torch.rand((1, 1, 28, 28)) # useful for logging the graph
+        self.example_input_array = torch.rand((1, 1, 28, 28))  # useful for logging the graph
 
         self.loss_fn = load_loss(
             hparams
