6bc361f505aa98ed553f9b7fb9777d12ff504694
Mirko Bronzi <m.bronzi@gmail.com>
2024-03-11 16:55:18 -0400

    fixed result check in Continuous Integration

--- tests/end2end_pytorch/run.sh
+++ tests/end2end_pytorch/run.sh
@@ -4,11 +4,13 @@ set -e
 # go to the examples folder and run the example
 cd $GITHUB_WORKSPACE/examples/local
 sh run.sh
-mv output outout_OLD
+mv output output_OLD
 # re-run the example to check reproducibility
 sh run.sh
 # check results are the same
-DIFF_LINES=`grep "best_dev_metric" output*/stats.yaml | sed 's@^.*best_dev_metric: @@g' | uniq | wc -l`
+echo "results are:"
+cat output*/results.txt
+DIFF_LINES=`cat output*/results.txt | uniq | wc -l`
 if [ ${DIFF_LINES} -gt 1 ]; then
     echo "ERROR: two identical runs produced different output results - review seed implementation"
     exit 1
@@ -23,8 +25,11 @@ mv orion_working_dir orion_working_dir_OLD
 # re-run the example to check reproducibility
 rm -fr orion_db*
 sh run.sh
-DIFF_LINES=`grep "best_dev_metric" orion_working_dir*/*/stats.yaml | sed 's@^.*best_dev_metric: @@g' | sort | uniq | wc -l`
-if [ ${DIFF_LINES} -gt 2 ]; then
+# check results are the same
+echo "results are:"
+cat orion_working_dir*/*/results.txt
+DIFF_LINES=`grep "best_dev_metric" orion_working_dir*/*/results.txt | sed 's@^.*best_dev_metric: @@g' | sort | uniq | wc -l`
+if [ ${DIFF_LINES} -gt 2 ]; then # note we have two trials per experiment, this is why we can have 2 different results - but not more
     echo "ERROR: two identical Orion runs produced different output results - review seed implementation"
     exit 1
 else
