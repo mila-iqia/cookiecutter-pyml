64042fe9cbf5fac82920deeca22d5049fe4b5e36
Jeremy Pinto <jerpint@gmail.com>
2024-08-15 12:22:27 -0400

    change entry point names globally

--- examples/local/run.sh
+++ examples/local/run.sh
@@ -1,3 +1,3 @@
 set -e
-amlrt-train --data ../data --output output --config ../config.yaml config.yaml --start-from-scratch
-amlrt-eval --data ../data --config ../config.yaml config.yaml --ckpt-path output/best_model/model.ckpt
+amlrt_project_train --data ../data --output output --config ../config.yaml config.yaml --start-from-scratch
+amlrt_project_eval --data ../data --config ../config.yaml config.yaml --ckpt-path output/best_model/model.ckpt
--- examples/local_orion/run.sh
+++ examples/local_orion/run.sh
@@ -2,8 +2,8 @@ set -e
 export ORION_DB_ADDRESS='orion_db.pkl'
 export ORION_DB_TYPE='pickleddb'
 
-merge-configs --config ../config.yaml config.yaml --merged-config-file merged_config.yaml
-orion -vvv -v hunt --config orion_config.yaml amlrt-train --data ../data \
+amlrt_project_merge_configs --config ../config.yaml config.yaml --merged-config-file merged_config.yaml
+orion -vvv -v hunt --config orion_config.yaml amlrt_project_train --data ../data \
     --config merged_config.yaml --disable-progressbar \
     --output '{exp.working_dir}/{trial.id}/' \
     --log '{exp.working_dir}/{trial.id}/exp.log'
