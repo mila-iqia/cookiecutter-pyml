2a5ab241146cc0b13ccb865109b110a0a92e27eb
Mirko Bronzi <m.bronzi@gmail.com>
2024-09-10 16:52:51 -0400

    fixed parsing bug

--- tests/end2end_pytorch/run_single.sh
+++ tests/end2end_pytorch/run_single.sh
@@ -22,11 +22,12 @@ fi
 EVAL_RESULT=`sh eval.sh | grep "Validation Metrics"`
 CLEANED_EVAL_RESULT=`echo $EVAL_RESULT | sed 's/.*: //g' | sed 's/}.*//g'`
 TRAIN_RESULT=`cat output/results.txt`
+CLEANED_TRAIN_RESULT=`echo TRAIN_RESULT | sed 's/.*: //g'`
 
-echo "train results: ${TRAIN_RESULT} / eval results: ${CLEANED_EVAL_RESULT}"
+echo "train results: ${CLEANED_TRAIN_RESULT} / eval results: ${CLEANED_EVAL_RESULT}"
 
 # Compare the two values, formatted to 5 decimal places
-if ! [ "$(printf "%.5f" "$CLEANED_EVAL_RESULT")" = "$(printf "%.5f" "$TRAIN_RESULT")" ]; then
+if ! [ "$(printf "%.5f" "$CLEANED_EVAL_RESULT")" = "$(printf "%.5f" "$CLEANED_TRAIN_RESULT")" ]; then
   echo "results are NOT equal up to 5 decimal places."
   exit 1
 else
