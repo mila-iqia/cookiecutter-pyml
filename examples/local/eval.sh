6bca2faad096094023646ef1bd9d6bab7a128012
Mirko Bronzi <m.bronzi@gmail.com>
2024-09-11 14:46:44 -0400

    fix to address an update in develop

--- examples/local/eval.sh
+++ examples/local/eval.sh
@@ -1,1 +1,1 @@
-amlrt-eval --data ../data --config ../config.yaml config.yaml --ckpt-path output/best_model/model.ckpt
+amlrt_project_eval --data ../data --config ../config.yaml config.yaml --ckpt-path output/best_model/model.ckpt
